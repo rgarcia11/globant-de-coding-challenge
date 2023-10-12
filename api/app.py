a52a474e2798983866817c6ff4bad99c30f9a112
Rogelio Garcia Escallon <rogelio.garciae@perficient.com>
2023-10-11 21:24:46 -0500

    2: Add first version of the api to upload CSV files for each table

--- api/app.py
+++ api/app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from flask_migrate import Migrate
-from sqlalchemy import create_engine
+import sqlalchemy as sa
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # Configure the database connection
 db_uri = 'postgresql://username:password@localhost/company_db'
-engine = create_engine(db_uri)
+engine = sa.create_engine(db_uri)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
 Migrate(app, db)
@@ -57,6 +57,93 @@ class Job(db.Model):
 
     def __repr__(self):
         return f"Employee(id={self.id}, job='{self.job}'"
+
+
+@app.route('/upload_employees', methods=['POST'])
+def upload_employees():
+    file = request.files['csv_file']
+    if file:
+        if file.filename.endswith('.csv'):
+            csv_data = file.read().decode('utf-8').splitlines()
+            for row in csv_data:
+                values = row.split(',')
+                sql = """
+                INSERT INTO employees (id, name, datetime, department_id, job_id)
+                VALUES (:id, :name, :datetime, :department_id, :job_id);
+                """
+                data = {
+                    "id": values[0],
+                    "name": values[1],
+                    "datetime": values[2],
+                    "department_id": values[3],
+                    "job_id": values[4]
+                }
+                try:
+                    db.session.execute(sa.text(sql), params=data)
+                    db.session.commit()
+                except Exception as e:
+                    return jsonify({"error": f"Failed to insert data: {str(e)}"})
+            return jsonify({"message": "File uploaded and data inserted successfully"})
+        else:
+            return jsonify({"error": "Please upload a CSV file"})
+    else:
+        return jsonify({"error": "No file selected"})
+
+
+@app.route('/upload_jobs', methods=['POST'])
+def upload_jobs():
+    file = request.files['csv_file']
+    if file:
+        if file.filename.endswith('.csv'):
+            csv_data = file.read().decode('utf-8').splitlines()
+            for row in csv_data:
+                values = row.split(',')
+                sql = """
+                INSERT INTO jobs (id, job)
+                VALUES (:id, :job);
+                """
+                data = {
+                    "id": values[0],
+                    "job": values[1]
+                }
+                try:
+                    db.session.execute(sa.text(sql), params=data)
+                    db.session.commit()
+                except Exception as e:
+                    return jsonify({"error": f"Failed to insert data: {str(e)}"})
+            return jsonify({"message": "File uploaded and data inserted successfully"})
+        else:
+            return jsonify({"error": "Please upload a CSV file"})
+    else:
+        return jsonify({"error": "No file selected"})
+
+
+@app.route('/upload_departments', methods=['POST'])
+def upload_departments():
+    file = request.files['csv_file']
+    if file:
+        if file.filename.endswith('.csv'):
+            csv_data = file.read().decode('utf-8').splitlines()
+            for row in csv_data:
+                values = row.split(',')
+                sql = """
+                INSERT INTO departments (id, department)
+                VALUES (:id, :department);
+                """
+                data = {
+                    "id": values[0],
+                    "department": values[1],
+                }
+                try:
+                    db.session.execute(sa.text(sql), params=data)
+                    db.session.commit()
+                except Exception as e:
+                    return jsonify({"error": f"Failed to insert data: {str(e)}"})
+            return jsonify({"message": "File uploaded and data inserted successfully"})
+        else:
+            return jsonify({"error": "Please upload a CSV file"})
+    else:
+        return jsonify({"error": "No file selected"})
 
 
 if __name__ == '__main__':
