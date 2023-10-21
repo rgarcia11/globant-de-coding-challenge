2615443d5208eb50251f4a6d854ee589250f2824
Rogelio Garcia <rogelio.garciae@perficient.com>
2023-10-21 14:12:06 -0500

    6: Finished setting up Cloud Run deployment

--- Dockerfile
+++ Dockerfile
@@ -20,7 +20,7 @@ ENV GOOGLE_APPLICATION_CREDENTIALS=/app/key.json
 COPY . /app
 
 # Expose the port that Flask will run on
-EXPOSE 5001
+EXPOSE 8080
 
 # Define the command to run the Flask application
-CMD ["flask", "run", "--host=0.0.0.0", "--port=5001"]
+CMD ["flask", "run", "--host=0.0.0.0", "--port=8080"]
--- api/challenge_app/app.py
+++ api/challenge_app/app.py
@@ -1,16 +1,13 @@
-# import sqlalchemy as sa
+import os
+
 from flask_migrate import Migrate
 
 from api.challenge_app.app_factory import create_app, config_blueprints
-from api.challenge_app.db_singleton import Base, db
-
+from api.challenge_app.db_singleton import db
 
 app = create_app(config_name='development')
 config_blueprints(app)
 migrate = Migrate(app, db)
 
-# Configure the database connection
-# engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
